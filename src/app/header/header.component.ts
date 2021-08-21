7911d1ffb5546b07b8e20a1cf1b507d9fd9edb7b
Eduardo David Muñoz G <eduardodavidmunozg@gmail.com>
2021-08-21 04:28:56 -0500

    Ejercicios de landing page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+
+  async bookAplace(){
+    const { value: formValues } = await Swal.fire({
+      title: 'Agenda una cita',
+      html:
+        '<labe>Nombre</label> <input id="swal-input1" class="swal2-input" type="text">' +
+        '<labe>Correo</label> <input id="swal-input2" class="swal2-input" type="email">' +
+        '<labe>Mensaje</label><input id="swal-input3" class="swal2-input" type="text" placeholder="Dejanos tus comentarios aqui.">' +
+        '<br><span>Te contactaremos lo antes posible.</span>',
+      focusConfirm: false,
+      showCancelButton: true,
+      preConfirm: () => {
+        return [
+          (<HTMLInputElement>document.getElementById('swal-input1')).value,
+          (<HTMLInputElement>document.getElementById('swal-input2')).value,
+          (<HTMLInputElement>document.getElementById('swal-input3')).value
+        ]
+      }
+    })
+    
+    if (formValues) {
+      Swal.fire("Perfecto!","Ya tenemos tus datos","success");
+      console.log(formValues);
+    }
+  }
 
   ngOnInit() {
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,13 +10,32 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-    Swal.fire({
-      title: 'Hola mundo!',
-      text: 'Do you want to continue',
-      icon: 'success',
-      confirmButtonText: 'Cool'
+  async bookAplace(){
+    const { value: formValues } = await Swal.fire({
+      title: 'Agenda una cita',
+      html:
+        '<labe>Nombre</label> <input id="swal-input1" class="swal2-input" type="text">' +
+        '<labe>Correo</label> <input id="swal-input2" class="swal2-input" type="email">' +
+        '<labe>Mensaje</label><input id="swal-input3" class="swal2-input" type="text" placeholder="Dejanos tus comentarios aqui.">' +
+        '<br><span>Te contactaremos lo antes posible.</span>',
+      focusConfirm: false,
+      showCancelButton: true,
+      preConfirm: () => {
+        return [
+          (<HTMLInputElement>document.getElementById('swal-input1')).value,
+          (<HTMLInputElement>document.getElementById('swal-input2')).value,
+          (<HTMLInputElement>document.getElementById('swal-input3')).value
+        ]
+      }
     })
+    
+    if (formValues) {
+      Swal.fire("Perfecto!","Ya tenemos tus datos","success");
+      console.log(formValues);
+    }
+  }
+
+  ngOnInit() {
   }
 
 }
